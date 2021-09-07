8693952f0554f24927af25b84badef79e746b27b
Stan Misina <sjmisina@gmail.com>
2021-09-07 15:11:35 -0400

    continue rebuild of logic.js

--- static/js/logic.js
+++ static/js/logic.js
@@ -4,19 +4,25 @@ console.log("working");
 // Create the map object with a center and zoom level.
 let map = L.map('mapid').setView([40.7, -94.5], 4);
 
-// map styles
-// mapbox/streets-v11
-// mapbox/outdoors-v11
-// mapbox/light-v10
-// mapbox/dark-v10
-// mapbox/satellite-v9
-// mapbox/satellite-streets-v11
+//  Add a marker to the map for Los Angeles, California.
+L.circleMarker([34.0522, -118.2437], {
+    radius: 10,
+    color: "black",
+    fillColor: "yellow"
+ }).addTo(map);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/streets-v11',
+    id: 'mapbox/dark-v10',
+    // map styles id's
+    // mapbox/streets-v11
+    // mapbox/outdoors-v11
+    // mapbox/light-v10
+    // mapbox/dark-v10
+    // mapbox/satellite-v9
+    // mapbox/satellite-streets-v11
     tileSize: 512,
     zoomOffset: -1,
     accessToken: API_KEY
