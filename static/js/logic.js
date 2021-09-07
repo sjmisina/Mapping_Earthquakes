deb3fe740d65dc2cd5081f6c1e272891f6e5aa2f
Stan Misina <sjmisina@gmail.com>
2021-09-07 17:00:19 -0400

    rebuilt logic through 13.5.5

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,54 +1,61 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-// Create the map object with center and zoom level.
-let map = L.map('mapid').setView([30, 30], 2);
-
-// // Get data from cities.js
-// let cityData = cities;
-
-// // Loop through the cities array and create one marker for each city.
-// cityData.forEach(function(city) {
-//     console.log(city)
-//     L.marker(city.location).addTo(map);
-// });
-
-// // We create the tile layer that will be the background of our map.
-// let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-//     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-//     maxZoom: 18,
-//     id: 'mapbox/streets-v11',
-//     // map styles id's
-//     // mapbox/streets-v11
-//     // mapbox/outdoors-v11
-//     // mapbox/light-v10
-//     // mapbox/dark-v10
-//     // mapbox/satellite-v9
-//     // mapbox/satellite-streets-v11
-//     tileSize: 512,
-//     zoomOffset: -1,
-//     accessToken: API_KEY
-// });
-
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    accessToken: API_KEY
+    accessToken: API_KEY,
+    id: 'mapbox/streets-v11',
 });
 
-// Then we add our 'streets' tile layer to the map.
-streets.addto(map);
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY,
+    id: 'mapbox/satellite-streets-v11',
+});
 
+// Create a base layer that holds both maps.
+let baseMaps = {
+    Street: streets,
+    Satellite: satelliteStreets
+};
+
+let map = L.map('mapid', {
+    center: [44.0, -80.0],
+    zoom: 2,
+    layers: [streets]
+})
+// Accessing the Toronto airline routes GeoJSON URL.
+let torontoData = "static/js/torontoRoutes.json";
 // Accessing the airport GeoJSON URL
-let airportData = "https://raw.githubusercontent.com/sjmisina/Mapping_Earthquakes/static/js/majorAirports.json";
+let airportData = "static/js/majorAirports.json"
 
-// // Then we add our 'graymap' tile layer to the map.
-// streets.addTo(map);
+// Create a style for the lines.
+let myStyle = {
+    color: "#ffffa1",
+    weight: 2
+};
 
 // Grabbing our GeoJSON data.
-d3.json(airportData).then(function(data) {
+d3.json(torontoData).then(function(data) {
     console.log(data);
   // Creating a GeoJSON layer with the retrieved data.
-  L.geoJson(data).addTo(map);
+  L.geoJson(data, {
+      style: myStyle
+      onEachFeature: function(feature, layer) {
+          layer.bindPopup("<h3> Airline: " +feature.properties.airline + "</h3> <hr> <h3> Destination: "
+          + feature.properties.dst + "</h3>");
+      }
+      .addTo(map),
 });
+
+// Create a style for the lines.
+let myStyle = {
+    color: "#ffffa1",
+    weight: 2
+};
+
+// Pass our map layers into our layer control and add the layer control to the map
+L.control.layers(baseMaps).addTo(map);
